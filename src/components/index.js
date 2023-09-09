cc219f0fa7852a99afc240f52efbd873a13ee891
Медведев Иван <ranzar2000@gmail.com>
2023-09-09 22:27:17 +0300

    Create an layout for app

---
+++ src/components/index.js
@@ -0,0 +1,24 @@
+import Navbar from './Navbar';
+import Billing from './Billing';
+import Business from './Business';
+import CardDeal from './CardDeal';
+import Clients from './Clients';
+import CTA from './CTA';
+import Footer from './Footer';
+import GetStarted from './GetStarted';
+import Hero from './Hero';
+import Stats from './Stats';
+import Testimonials from './Testimonials';
+
+export{
+    Navbar,
+    Billing,
+    Business,
+    CardDeal,
+    Clients,
+    CTA,
+    Footer,
+    Hero,
+    Stats,
+    Testimonials,
+};
